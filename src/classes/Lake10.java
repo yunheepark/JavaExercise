a6831f2e242eb6f98fa57dd6fe0c8c798a106415
Junyoung Heo <jyheo0@gmail.com>
2016-04-04 16:26:31 +0900

    Fish inhertiance, move() dynamic binding

--- src/classes/Lake10.java
+++ src/classes/Lake10.java
@@ -3,62 +3,43 @@ package classes;
 import java.util.Scanner;
 
 /**
- * Created by jyheo on 2016-04-03.
+ * Created by jyheo on 2016-04-04.
  */
-class Fish {
-    private String name;
-    private String shape;
-    protected int x, y;
-
-    public Fish(String name, String shape) {
-        this.name = name;
-        this.shape = shape;
-        x = 0;
-        y = 0;
-    }
-
-    public Fish() {
-        this.name = "Unknown";
-        this.shape = "<--<";
-        x = 0;
-        y = 0;
+class FoolFish extends Fish {
+    FoolFish(String name, String shape) {
+        super(name, shape);
     }
 
     public void move(int width, int height) {
-        double rand = Math.random();
-        if (rand < 0.5)
-            x++;
-        else
-            y++;
+        x++;
+        y++;
         if (x >= width)
             x = 0;
         if (y >= height)
             y = 0;
     }
-
-    public void display(int x, int y) {
-        if (this.x == x && this.y == y) {
-            System.out.print(shape);
-        }
-    }
 }
 
-public class Lake {
+public class Lake10 {
     private int width;
     private int height;
-    private Fish fish;
-    private Fish fish2;
+    private Fish[] fishes;
 
-    public Lake(int width, int height) {
+    public Lake10(int width, int height) {
         this.width = width;
         this.height = height;
-        fish = new Fish();
-        fish2 = new Fish("p", "<***<");
+        fishes = new Fish[10];
+        for (int i = 0; i < fishes.length; i++) {
+            if (i % 2 == 0)
+                fishes[i] = new Fish("Even", "<#--<");
+            else
+                fishes[i] = new FoolFish("Odd", "<$--<");
+        }
     }
 
     public void moveFish() {
-        fish.move(width, height);
-        fish2.move(width, height);
+        for (Fish f : fishes)
+            f.move(width, height);
     }
 
     public void display() {
@@ -68,8 +49,8 @@ public class Lake {
         for (int i = 0; i < height; i++) {
             System.out.print("|");
             for (int j = 0; j < width; j++) {
-                fish.display(j, i);
-                fish2.display(j, i);
+                for (Fish f : fishes)
+                    f.display(j, i);
                 System.out.print(" ");
             }
             System.out.println("|");
@@ -80,7 +61,7 @@ public class Lake {
     }
 
     public static void main(String args[]) {
-        Lake lake = new Lake(80, 20);
+        Lake10 lake = new Lake10(80, 20);
         Scanner scanner = new Scanner(System.in);
         while (true) {
             lake.moveFish();
